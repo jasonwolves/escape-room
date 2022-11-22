31b5555d5f9d53d63e6d3b3a32f22bf1400c16f2
jasonwolves <97819220+jasonwolves@users.noreply.github.com>
2022-11-21 21:25:47 -0800

    Create password.js

---
+++ password.js
@@ -0,0 +1,7 @@
+var pass1 = 3736.62417618;
+var input = document.getElementById("userInput").value;
+alert(input);
+
+if (input == pass1) {
+  document.getElementById("demo").innerHTML = "Hello JavaScript!";
+}
