e6714b00eebd0d4e2cca7abf83a56760877b0500
jasonwolves <97819220+jasonwolves@users.noreply.github.com>
2022-11-22 07:54:25 -0800

    Delete password.js

--- password.js
+++
@@ -1,8 +0,0 @@
-function othername() {
-    var pass1 = 3174;
-    var input = document.getElementById("userInput").value;
-
-    if (input == pass1) {
-        document.getElementById("hidden_clue").textContent = "Hello JavaScript!";
-    }
-}
